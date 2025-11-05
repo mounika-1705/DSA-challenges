3d6d5ef7f07bd0ece82620eb1981b598992c1422
mounika-1705 <siripurammounika1919@gmail.com>
2025-11-05 22:04:49 +0530

    Added day 70

---
+++ day71.js
@@ -0,0 +1,46 @@
+function solveNQueens(N) {
+  const board = Array.from({ length: N }, () => Array(N).fill('.')); // create board
+  const result = [];
+
+  function isSafe(row, col) {  //checks for safety
+    for (let i = 0; i < row; i++) {
+      if (board[i][col] === 'Q') return false;
+    }
+
+    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {  //checks diagonal
+      if (board[i][j] === 'Q') return false;
+    }
+
+    for (let i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++) {
+      if (board[i][j] === 'Q') return false;
+    }
+
+    return true;
+  }
+
+  function placeQueen(row) {
+    if (row === N) {
+      const copy = board.map(r => r.join(' '));
+      result.push(copy);
+      return;
+    }
+
+    for (let col = 0; col < N; col++) {
+      if (isSafe(row, col)) {
+        board[row][col] = 'Q';       
+        placeQueen(row + 1);         
+        board[row][col] = '.';    
+      }
+    }
+  }
+
+  placeQueen(q);
+
+  result.forEach((solution, index) => {
+    console.log(`Solution ${index + 1}:`);
+    solution.forEach(row => console.log(row));
+    console.log();
+  });
+}
+
+solveNQueens(4);
